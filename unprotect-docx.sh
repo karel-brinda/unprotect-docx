342402abd5b7877ce6db5720d213d892fd04f1b8
Karel Břinda <karel.brinda@gmail.com>
2023-05-28 00:21:15 +0200

    Add messages

--- unprotect-docx.sh
+++ unprotect-docx.sh
@@ -10,6 +10,19 @@ readonly PROGDIR=$(dirname $0)
 readonly -a ARGS=("$@")
 readonly NARGS="$#"
 
+C='\033[0;33m'
+NC='\033[0m'
+
+msg() {
+	set +x
+	echo
+	echo -e "${C}=================================================${NC}"
+	echo -e "${C}$@${NC}"
+	echo -e "${C}=================================================${NC}"
+	echo
+	set -x
+}
+
 if [[ $NARGS -ne 2 ]]; then
 	>&2 echo "usage: $PROGNAME protected.docx unprotected.docx"
 	exit 1
@@ -19,16 +32,22 @@ x="$1"
 y="$2"
 d=$(mktemp -d)
 
-echo "tmp dir: $d"
+set -x
+msg "||           $PROGNAME                 ||"
+msg "Step 1: creating a tmp dir:\n        \"$d\""
 
 cp "$x" "$d/a.docx"
 
 (
 	cd "$d"
 	mkdir a
+	msg "Step 2: unzipping the content of \"$x\" into\n        \"$d/a\""
 	unzip -d a a.docx
 
+	msg "Step 3: modifying the content"
 
+
+	msg "Step 4: re-zipping the content of\n         \"$d/a\"\n        into \"$y\""
 	(
 		cd a
 		zip ../b.docx $(find .)
@@ -37,3 +56,5 @@ cp "$x" "$d/a.docx"
 
 
 cp "$d/b.docx" "$y"
+
+msg Finished 🎉
